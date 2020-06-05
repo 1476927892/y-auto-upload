92520c89a405517de4178661ac08bdfee1db1c8e
佩奇 <1476927892@qq.com>
2020-06-05 18:19:12 +0800

    2

---
+++ index.js
@@ -0,0 +1,19 @@
+#!/usr/bin/env node
+"use strict";
+const fs = require("fs");
+const Path = require("path");
+let configPath = "../../yau.json";
+console.log(__filename, __dirname);
+// fs.readFile(pathFile, "utf-8", function (err, data) {
+//   if (err) {
+//     console.log(err, "读取服务器配置文件失败");
+//   } else {
+//     try {
+//       console.log("读取配置文件");
+//       let json = JSON.parse(data);
+//       console.log(json);
+//     } catch (error) {
+//       console.log(err, "读取服务器配置文件失败");
+//     }
+//   }
+// });
