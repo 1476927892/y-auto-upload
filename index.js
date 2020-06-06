dad592c705ec4d61ff772b547f6f24aceacff596
佩奇 <1476927892@qq.com>
2020-06-06 09:19:03 +0800

    2

--- index.js
+++ index.js
@@ -2,21 +2,7 @@
 "use strict";
 const fs = require("fs");
 const Path = require("path");
-const { cosmiconfig } = require("cosmiconfig");
-const explorer = cosmiconfig("yau.json");
-let configPath = "../../yau.json";
-explorer
-  .search()
-  .then((result) => {
-    // result.config is the parsed configuration object.
-    // result.filepath is the path to the config file that was found.
-    // result.isEmpty is true if there was nothing to parse in the config file.
-    console.log(result);
-  })
-  .catch((error) => {
-    // Do something constructive.
-    console.log(error);
-  });
+console.log(process.env);
 // fs.readFile(pathFile, "utf-8", function (err, data) {
 //   if (err) {
 //     console.log(err, "读取服务器配置文件失败");
